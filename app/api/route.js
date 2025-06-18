0a646310cb6ef4e97b0e55e0b24628a5a2576057
Aaron Mettler <aaron.mettler@gmx.ch>
2025-06-18 07:16:36 +0200

    chang eadd componetn
    strat bakcend

--- app/api/route.js
+++ app/api/route.js
@@ -7,3 +7,15 @@ export async function GET(request) {
     const data = await get_components_with_options();
     return NextResponse.json({data: data}, { status: 200 })
 }
+
+export async function POST(request) {
+
+    //const formData = await request.formData()
+    const data = await request.json();
+    if (!data || !data.component || !data.title || !data.description || !data.price || !data.shop || !data.link || !data.imageUrl) {
+        return NextResponse.json({error: "Fehlende erforderliche Felder"}, { status: 400 })
+    }
+
+    console.log("Received data:", data);
+    return NextResponse.json({data: data}, { status: 400 })
+}
--- components/addComponent.jsx
+++ components/addComponent.jsx
@@ -13,27 +13,131 @@ import { Label } from '@/components/ui/label'
 import { title } from 'process';
 import { Textarea } from './ui/textarea';
 import Image from 'next/image';
+import { toast } from 'sonner';
 
 const AddComponent = () => {
   const componentGroup = ["frame", "motors", "esc", "fc", "props", "battery", "camera"]
-  const shops = [
-    {id: 1, name: "Fpvracing", link: "https://fpvracing.ch"},
-    {id: 2, name: "FPVFrame", link: "https://fpvframe.ch"},
-    {id: 3, name: "Dronefactory", link: "https://dronefactory.ch"},
-    {id: 4, name: "FPV24", link: "https://www.fpv24.com/de"},
-    {id: 5, name: "Quadmula", link: "https://quadmula.com/"}, ]
+  const shops = ["Fpvracing", "FPVFrame", "Dronefactory", "FPV24", "Quadmula"]
 
-  const [component, setComponent] = useState({component: "frame", title: "", description: "", price: "", shop: "", link: "", imageUrl: ""})
+  const [component, setComponent] = useState({component: "frame", title: "", description: "", price: "", shop: "", link: "", imageUrl: "", options: {}})
   const [hasError, setHasError] = useState(false);
+
+  const handleAddComponent = async (e) => {
+    e.preventDefault();
+    if (component.title.trim() === "" || component.description.trim() === "" || component.price <= 0 || component.shop.trim() === "" || component.link.trim() === "" || component.imageUrl.trim() === "") {
+      toast.error("Bitte füllen Sie alle Felder korrekt aus.");
+      return;
+    }
+
+    try {
+      const response = await fetch('/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(component),
+      });
+
+      if (!response.ok) {
+        toast.error('Fehler beim Hinzufügen der Komponente');
+        return;
+      }
+
+      const data = await response.json();
+      console.log('Komponente erfolgreich hinzugefügt:', data);
+      toast.success("Komponente erfolgreich hinzugefügt");
+    } catch (error) {
+      console.error('Fehler:', error);
+      toast.error("Fehler beim Hinzufügen der Komponente");
+    }
+  }
+
+  const handleComponentChange = (value) => {
+    setComponent(prev => ({ ...prev, component: value }));
+    switch (value) {
+      case "frame":
+        setComponent(prev => ({
+          ...prev,
+          options: {
+            frameSize: "",
+            frameType: "",
+            frameMaterial: ""
+          }
+        }));
+        break;
+      case "motors":
+        setComponent(prev => ({
+          ...prev,
+          options: {
+            motorSize: "",
+            motorKV: "",
+            motorType: ""
+          }
+        }));
+        break;
+      case "esc":
+        setComponent(prev => ({
+          ...prev,
+          options: {
+            escSize: "",
+            escType: "",
+            escAmperage: ""
+          }
+        }));
+        break;
+      case "fc":
+        setComponent(prev => ({
+          ...prev,
+          options: {
+            fcType: "",
+            fcSize: "",
+            fcFeatures: ""
+          }
+        }));
+        break;
+      case "props":
+        setComponent(prev => ({
+          ...prev,
+          options: {
+            propSize: "",
+            propMaterial: "",
+            propType: ""
+          }
+        }));
+        break;
+      case "battery":
+        setComponent(prev => ({
+          ...prev,
+          options: {
+            batterySize: "",
+            batteryType: "",
+            batteryCapacity: ""
+          }
+        }));
+        break;
+      case "camera":
+        setComponent(prev => ({
+          ...prev,
+          options: {
+            cameraType: "",
+            cameraResolution: "",
+            cameraFeatures: ""
+          }
+        }));
+        break;
+      default:
+        break;
+    }
+  }
 
   return (
     <div className='w-full px-[30%] pt-1'>
-      <form className='w-full' onSubmit={null}>
+      <form className='w-full' onSubmit={handleAddComponent}>
         <h1 className='text-4xl font-bold text-white'>Komponent Hinzufügen</h1>
         <div>
-          <Label className='text-white'>Wählen Sie eine Komponente aus:</Label>
+          <Label className='text-white'>Wählen Sie eine Komponente aus: *</Label>
           <Select
-            onValueChange={(value) => setComponent(prev => ({ ...prev, title: value }))}
+            onValueChange={handleComponentChange}
           >
             <SelectTrigger className="w-full p-2 border rounded-md mt-2">
               <SelectValue placeholder="-- Bitte wählen --" />
@@ -48,40 +152,35 @@ const AddComponent = () => {
           </Select>
         </div>
         <div className='my-2'>
-          <Label className='text-white'>Titel:</Label>
-          <Input required placeholder="Komponent Titel" onChange={(e) => setComponent(prev => ({ ...prev, title: e.target.value }))} value={component.title}/>
+          <Label className='text-white'>Titel: *</Label>
+          <Input placeholder="Komponent Titel" onChange={(e) => setComponent(prev => ({ ...prev, title: e.target.value }))} value={component.title}/>
           {component.title.trimEnd() === "" && (<p className='text-[#d9534f]'>Bitte geben sie einen Titel ein</p>) }
         </div>
         <div className='my-2 relative'>
-          <Label className='text-white'>Beschreibung:</Label>
+          <Label className='text-white'>Beschreibung: *</Label>
           <Textarea placeholder='Komponente Beschreibung' onChange={(e) => setComponent(prev => ({ ...prev, description: e.target.value }))} value={component.description}/>
           {component.description.trimEnd() === "" && (<p className='text-[#d9534f]'>Bitte geben sie eine Beschreibung ein</p>) }
         </div>
         <div className='my-2 relative'>
-          <Label className='text-white'>Preis:</Label>
-          <Input type="number" min="0" required onChange={(e) => setComponent(prev => ({ ...prev, price: e.target.value }))} value={component.price}/>
+          <Label className='text-white'>Preis: *</Label>
+          <Input type="number" min="0" onChange={(e) => setComponent(prev => ({ ...prev, price: e.target.value }))} value={component.price}/>
           {component.price == 0 && (<p className='text-[#d9534f]'>Bitte geben sie einen Preis ein</p>) }
           {component.price < 0 && (<p className='text-[#d9534f]'>Preis muss grösser als 0 sein</p>) }
         </div>
         <div className='my-2 relative'>
-          <Label className='text-white'>Shop:</Label>
+          <Label className='text-white'>Shop: *</Label>
           <Select
             onValueChange={(value) => {
-              const selectedShop = shops.find((shop) => shop.id.toString() === value);
-              setComponent((prev) => ({
-                ...prev,
-                shop: selectedShop?.name || '',
-                link: selectedShop?.link || '',
-              }));
+              setComponent((prev) => ({...prev, shop: value}));
             }}
           >
             <SelectTrigger className="w-full p-2 border rounded-md mt-2">
               <SelectValue placeholder="-- Bitte wählen --" />
             </SelectTrigger>
             <SelectContent>
-              {shops.map((shop) => (
-                <SelectItem key={shop.id} value={shop.id.toString()}>
-                  {shop.name}
+              {shops.map((shop, index) => (
+                <SelectItem key={index} value={shop}>
+                  {shop}
                 </SelectItem>
               ))}
             </SelectContent>
@@ -89,12 +188,13 @@ const AddComponent = () => {
           {component.shop.trimEnd() === "" && (<p className='text-[#d9534f]'>Bitte geben sie einen Shop ein</p>) }
         </div>
         <div className='my-2 relative'>
-          <Label className='text-white'>Link zum Shop:</Label>
-          <Input type="text" readOnly value={component.link}/>
+          <Label className='text-white'>Link zum Produkt: *</Label>
+          <Input type="text" value={component.link} onChange={(e) => setComponent(prev => ({...prev, link: e.target.value}))}/>
+          {component.link.trimEnd() === "" && (<p className='text-[#d9534f]'>Bitte geben sie einen Link zum Produkt ein</p>) }
         </div>
         <div className='my-2 relative'>
-          <Label className='text-white'>Bild url:</Label>
-          <Input type="text" required onChange={(e) => setComponent(prev => ({ ...prev, imageUrl: e.target.value }))} value={component.imageUrl}/>
+          <Label className='text-white'>Bild url: *</Label>
+          <Input type="text" onChange={(e) => setComponent(prev => ({ ...prev, imageUrl: e.target.value }))} value={component.imageUrl}/>
           {component.imageUrl.trimEnd() === "" && (<p className='text-[#d9534f]'>Bitte geben sie ein Bild url ein ein</p>) }
           {component.imageUrl.trimEnd() !== "" && (
             <Image
@@ -107,7 +207,38 @@ const AddComponent = () => {
               onLoadingComplete={() => setHasError(false)}/>
           )}
         </div>
-        <Button className='w-full mt-4' type='submit'>Login</Button>
+        <div className='my-2 relative'>
+          <Label className='text-white'>Optionen: *</Label>
+          {component.options && Object.keys(component.options).length > 0 ? (
+            Object.entries(component.options).map(([key, value]) => (
+              <div key={key}>
+                <div className="my-2 relative flex items-center gap-2">
+                  <Label className="text-white w-25">{key}</Label>
+                  <Input
+                    type="text"
+                    value={value || ''} // Wert der Option (nicht component.options.option)
+                    onChange={(e) =>
+                      setComponent((prev) => ({
+                        ...prev,
+                        options: {
+                          ...prev.options,
+                          [key]: e.target.value, // Aktualisiere den Wert für den spezifischen Schlüssel
+                        },
+                      }))
+                    }
+                    className="p-2"
+                  />
+                </div>
+                {value.trimEnd() === '' && (
+                  <p className="text-[#d9534f] ml-25">Bitte geben Sie einen Wert ein</p>
+                )}
+              </div>
+            ))
+          ) : (
+            <p className="text-gray-400">Keine Optionen hinzugefügt</p>
+          )}
+        </div>
+        <Button className='w-full mt-4' type='submit'>Kompnent hinzufügen</Button>
       </form>
     </div>
   )
